c8b579698b56b61110866ffbf14bc1035c36ca90
josmorales1 <josgerardomorales1@aol.com>
2020-02-25 16:16:00 -0600

    updated navbar

--- clickygame/src/components/Game/index.js
+++ clickygame/src/components/Game/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Nav from "../Nav";
 import Header from "../Header";
 import Container from "../Container";
-import ClickItem from "../ClickItem";
+import ClickItem from "../ ClickItem";
 import Footer from "../Footer";
 import data from "../../data.json";
 
--- clickygame/src/components/Header/index.js
+++ clickygame/src/components/Header/index.js
@@ -4,8 +4,8 @@ import "./style.css";
 function Header() {
     return (
         <header className="header">
-            <h1>Clicky Game!</h1>
-            <h2>Click on an image to earn points, but don't click on any more than once!</h2>
+            <h1>Clicky Game Football Edition</h1>
+            <h2>Click on a football club crest to earn points, but try to not click it more than once!</h2>
         </header>
     );
 }
