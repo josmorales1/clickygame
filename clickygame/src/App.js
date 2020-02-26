ce9d5aa592c677dbdcd21edff87cf5d461ba1a35
josmorales1 <josgerardomorales1@aol.com>
2020-02-25 19:59:22 -0600

    updated file

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -2,12 +2,7 @@ import React from "react";
 import Game from "./components/Game";
 
 
-app.use(express.static(path.join(__dirname, 'build')));
 
-
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
 
 function App() {
     return <Game />;
